51b2f9cbc19e35266784426911f13386f6d8e906
Tevin Aduma <Tevin Aduma>
2017-02-21 15:35:14 +0300

    refractoring the project to find whether a year is alleap year by figuring out if it is divisible by both 4 and not 100 and 400

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
         event.preventDefault();
 
         var leapYear = function(year) {
-            if ((year % 4 === 0) && (year % 100 === 0)) {
-                return false;
+            if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+                return true;
             } else {
                 return false;
             }
